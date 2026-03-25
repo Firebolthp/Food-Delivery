e304b4b2a9ed6ae4295184e90579e0fcab39bb55
Firebolthp <rdevansh528@gmail.com>
2026-03-26 03:39:17 +0530

    feat: implement backend APIs for shop and item management with controllers and routes

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import connectDb from "./config/db.js"
 import cookieParser from "cookie-parser"
 import authRouter from "./routes/auth.routes.js"
 import userRouter from "./routes/user.routes.js"
+import itemRouter from "./routes/item.routes.js"
+import shopRouter from "./routes/shop.routes.js"
 import cors from "cors"
 
 
@@ -12,13 +14,15 @@ const app=express()
 const port=process.env.PORT || 5000
 
 app.use(cors({
-     origin:"http://localhost:5174",
-    credentials:true,
-}))
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+}));
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
+app.use("/api/shop",shopRouter)
+app.use("/api/item",itemRouter)
 
 app.listen(port,()=>{
     connectDb()
